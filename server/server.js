07f96eb14d5d5669b02d84e6b371c2c063dde9cc
Aditya Vishwakarma <vishwakarmaaditya471@gmail.com>
2025-07-23 12:06:57 +0530

    fixed

--- server/server.js
+++ server/server.js
@@ -3,17 +3,25 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const path = require("path");
+const mongoSanitize = require('express-mongo-sanitize');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  credentials: true,
+}));
 
-app.use('/images', express.static(path.join(__dirname, 'public/images')));
+app.use(express.json({ limit: '50mb' })); // Prevent large payload attacks
 
-app.use('/games', express.static(path.join(__dirname, 'games')));
+app.use('/images', express.static(path.join(__dirname, 'public/images'), { dotfiles: 'deny' }));
+
+app.use('/games', express.static(path.join(__dirname, 'games'), { dotfiles: 'deny' }));
+
+app.use(mongoSanitize());
 
 // Test route
 app.get("/", (req, res) => {
